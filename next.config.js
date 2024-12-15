5c44cafc890c4cbb065106b1984db8765514ea8d
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-12-15 12:33:15 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'secretgreen9.s3.ap-southeast-2.amazonaws.com',
+        pathname: '/thumbnails/products/**',
+      },
+    ],
+  },
+};
